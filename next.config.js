ec2273a811968ace91f62819db12d4fb99046722
TheFatAndFurious <mrguerrilla@gmail.com>
2023-08-03 00:30:52 +0200

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },}
 
 module.exports = nextConfig
