cd6d81566483451aab4cca616ff43513b318adbd
TheFatAndFurious <mrguerrilla@gmail.com>
2023-08-17 23:27:47 +0200

    added multiple files upload

--- src/app/admin/upload/page.tsx
+++ src/app/admin/upload/page.tsx
@@ -1,5 +1,15 @@
-export default function(){
+import FileUploadForm from "@/app/components/admin/FileUploadForm";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export default async function(){
+
+    const supabase = createServerComponentClient({ cookies });
+
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
     return (
-        <h1>coucou j'uploade</h1>
+        <FileUploadForm />
     )
 }
--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,9 +1,6 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
 import { NextResponse } from "next/server"
 
-
-
-
 export async function POST(req: Request)  {
     const formData = await req.formData()
     const formDataEntryValues = Array.from(formData.values());
@@ -13,25 +10,26 @@ export async function POST(req: Request)  {
         if(typeof formDataEntryValue === "object" && "arrayBuffer" in formDataEntryValue) {
              file = formDataEntryValue as unknown as Blob;
              buffer = Buffer.from(await file.arrayBuffer())
+             const client = new S3Client({
+                 region: process.env.AWS_REGION,
+             })
+                const command = new PutObjectCommand ({
+                    Bucket: process.env.AWS_BUCKET_NAME,
+                    Key: file?.name,
+                    Body: buffer
+                })
+             
+                try {
+                    const response = await client.send(command)
+                    return NextResponse.json({ success: true });
+
+                } catch(error) {
+                    console.error(error)
+                    return NextResponse.json({ success: false})
+                }
         }
 
     }
     
-    const client = new S3Client({
-        region: process.env.AWS_REGION,
-    })
-       const command = new PutObjectCommand ({
-           Bucket: process.env.AWS_BUCKET_NAME,
-           Key: file?.name,
-           Body: buffer
-       })
-    
-       try {
-           const response = await client.send(command)
-           console.log(response)
-       } catch(error) {
-           console.error(error)
-       }
 
-    return NextResponse.json({ success: true });
 }
